385b5c90767ca91420b2f071942e2dcaafd779dc
xunyi24 <liupf24@163.com>
2020-07-30 21:44:23 +0800

    project structure initialization

---
+++ mall-order/src/test/java/cn/xunyi/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xunyi.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
